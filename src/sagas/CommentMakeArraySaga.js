a9f9efa1e896444484b79a89fc2c4429106c779c
Paritta <paritta@Parittaui-MacBook-Pro.local>
2017-11-19 16:24:01 +0900

    Add CommentMakeArray saga

--- src/sagas/CommentMakeArraySaga.js
+++ src/sagas/CommentMakeArraySaga.js
@@ -1,7 +1,7 @@
 import { put, takeEvery } from "redux-saga/effects/";
 import { getFirebase } from "react-redux-firebase";
 
-function* CommentReceive (action) {
+function* CommentMakeArray (action) {
     try {
         const data = yield getFirebase()
             .database()
@@ -11,13 +11,13 @@ function* CommentReceive (action) {
                 return res.val();
             });
 
-        yield put({ type: "comment/comment_receive_Success" });
+        yield put({ type: "comment/comment_make_array_Success" });
     } catch (error) {
-        yield put({ type: "comment/comment_receive_Failure", payload: error })
+        yield put({ type: "comment/comment_make_array_Failure", payload: error })
     }
 }
 
-export function* watchCommentReceive () {
-    yield takeEvery("comment/comment_receive_Request", CommentReceive);
+export function* watchCommentMakeArray () {
+    yield takeEvery("comment/comment_make_array_Request", CommentMakeArray);
 }
 
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,12 +2,14 @@ import { all } from "redux-saga/effects";
 import { watchFetchData } from "./FetchSaga";
 import { watchCommentSend } from "./CommentSendSaga";
 import { watchCommentReceive } from "./CommentReceiveSaga";
+import { watchCommentMakeArray } from "./CommentMakeArraySaga";
 
 export default function* rootSaga () {
     yield all([
         watchCommentSend(),
         watchFetchData(),
-        watchCommentReceive()
+        watchCommentReceive(),
+        watchCommentMakeArray(),
     ])
 }
 
